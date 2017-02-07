96661b948b1793c23fbed09257c6fa4b4545010c
BoakyeDankwa <boakye.dankwa@controlsdata.com>
2017-02-07 10:11:20 -0500

    Initial commit

---
+++ src/DataStructures.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : DataStructures.c
+ Author      : Boakye Dankwa
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
